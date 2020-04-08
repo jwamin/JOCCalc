9e42c12765304a2bcd209574b277fbdbe63e5b8d
Joss Manger <jossferatu@gmail.com>
2020-04-08 03:58:12 -0400

    Initial Commit

---
+++ CCalc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CCalc
+//
+//  Created by Joss Manger on 4/8/20.
+//  Copyright © 2020 Joss Manger. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
